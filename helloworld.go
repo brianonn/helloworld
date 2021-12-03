f69ddae447d28a09ae72bd55c72fc7aa59147790
Brian Onn <brianonn@users.noreply.github.com>
2021-12-02 22:24:51 -0800

    add date and time to output

--- helloworld.go
+++ helloworld.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World from Go in minimal Docker container")
+	fmt.Fprintf(w, "[ %s ] Hello World from Go in minimal Docker container\r\n", currentTime.Format("2006-01-02 15:04:05"))
 }
 
 func main() {
